815bbc39ca55aacbd84ead64b93ca61b0f896d0f
Alejandro Garcia Barajas <agarciab28@gmail.com>
2017-10-31 08:47:09 -0600

    Practica Convolucion Fin

--- Graficacion/Practicas/Practica Convolucion/convolucion.cpp
+++ Graficacion/Practicas/Practica Convolucion/convolucion.cpp
@@ -10,12 +10,13 @@ using namespace cv;
 
 int main(int argc, char const *argv[]) {
   Mat img; //Lienzo de imagen original
-  int i, j;
+  int i, j, aux;
   int conv[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}; //Matriz de convolucion
-  img = imread("lunala.jpg", 0); //Lectura de imagen
+  img = imread("mario.jpg", 0); //Lectura de imagen
 
   Mat img2(img.rows * 2, img.cols * 2, CV_8UC1, Scalar(250)); //Lienzo de imagen con escalamiento
-  Mat img3(img.rows * 2, img.cols * 2, CV_8UC1, Scalar(250)); //Lienzo de imagen aplicando matriz de convolucion
+  Mat img3(img2.rows + 2, img2.cols + 2, CV_8UC1, Scalar(250)); //Lienzo de imagen aplicando matriz de convolucion
+  Mat img4(img.rows * 2, img.cols * 2, CV_8UC1, Scalar(250));
 
   //Ciclo de escalamiento
   for(i = 0; i < img.rows; i++){
@@ -24,10 +25,36 @@ int main(int argc, char const *argv[]) {
     }
   }
 
+  //Mover Imagen
+  for(i = 0; i < img2.rows; i++){
+    for(j = 0; j < img2.cols; j++){
+      img3.at<uchar>(i + 1, j + 1) = img2.at<uchar>(i, j);
+    }
+  }
+
+  //Convolucion
+  for(i = 1; i < img3.rows - 1; i++){
+    for(j = 1; j < img3.cols - 1; j++){
+      aux = conv[0][0] * img3.at<uchar>(i - 1, j + 1);
+		  aux = aux + conv[1][0] * img3.at<uchar>(i, j + 1);
+		  aux = aux + conv[2][0] * img3.at<uchar>(i + 1, j + 1);
+		  aux = aux + conv[0][1] * img3.at<uchar>(i - 1, j);
+		  aux = aux + conv[1][1] * img3.at<uchar>(i, j);
+		  aux = aux + conv[2][1] * img3.at<uchar>(i + 1, j);
+		  aux = aux + conv[0][2] * img3.at<uchar>(i - 1, j - 1);
+		  aux = aux + conv[1][2] * img3.at<uchar>(i, j - 1);
+		  aux = aux + conv[2][2] * img3.at<uchar>(i + 1, j - 1);
+		  img4.at<uchar>(i - 1, j - 1) = aux;
+    }
+  }
+
 
   imshow("Imagen", img);
   waitKey(0);
-  imshow("Escalamiento", img2);
+  imshow("Escalamiento", img3);
   waitKey(0);
+  imshow("Convolucion", img4);
+  waitKey(0);
+
   return 0;
 }
